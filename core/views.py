540da33865d067ea230a17007de935beb84e698f
Long Nguyen <longnh140200@gmail.com>
2022-11-13 20:26:27 +0700

    add all files

---
+++ core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from store.models import Product
+
+# Create your views here.
+
+def homepage(request):
+    context = {}
+    products = Product.objects.filter(status=Product.ACTIVE)[0:6]
+    if products is not None:
+        context = {
+            'products': products,
+        }
+    return render(request, 'core/homepage.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'core/about.html', context)
